5dc200433a05a9b64041df96a6c8b067cf784cee
praveenkumarulli <praveenulli2006@gmail.com>
2026-04-17 10:37:42 +0530

    UC8:Filter Passenger Bogies Using Streams

---
+++ src/TrainConsistApp.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + capacity + " seats";
+    }
+}
